9f3a6cdc8ce0a301a2ed6149e663e02ac7a9e0aa
Jonas Westenhoff <jwestenhoff@web.de>
2021-04-09 21:51:02 +0200

    fit default setup script

--- ubuntu/scripts/doDefaultSetup.sh
+++ ubuntu/scripts/doDefaultSetup.sh
@@ -1,51 +1,60 @@
-# arguments.sh
-
 # Default values of arguments
 INTERNET=true
 IS_LAPTOP=false
 IS_DUAL_BOOT=false
 OTHER_ARGUMENTS=()
 
-# Loop through arguments and process them
-for arg in "$@"
-do
-    case $arg in
-        -ni|--no-internet)
-        INTERNET=false
-        shift # Remove argument from processing
-        ;;
-        -l|--laptop)
-        IS_LAPTOP=true
-        shift # Remove argument from processing
-        ;;  
-        -db|--dual-boot)
-        IS_DUAL_BOOT=true
-        shift # Remove argument from processing
-        ;;      
-        *)
-        OTHER_ARGUMENTS+=("$1")
-        shift # Remove argument from processing
-        ;;
-    esac
+echo "# read and process arguments"
+for arg in "$@"; do
+  case $arg in
+  -ni | --no-internet)
+    INTERNET=false
+    shift # Remove argument from processing
+    ;;
+  -l | --laptop)
+    IS_LAPTOP=true
+    shift # Remove argument from processing
+    ;;
+  -db | --dual-boot)
+    IS_DUAL_BOOT=true
+    shift # Remove argument from processing
+    ;;
+  *)
+    OTHER_ARGUMENTS+=("$1")
+    shift # Remove argument from processing
+    ;;
+  esac
 done
 
-echo "# internet: $INTERNET"
-echo "# is laptop: $IS_LAPTOP"
-echo "# is dual boot: $IS_DUAL_BOOT"
-echo "# other arguments: ${OTHER_ARGUMENTS[*]}"
+echo " - internet: $INTERNET"
+echo " - is laptop: $IS_LAPTOP"
+echo " - is dual boot: $IS_DUAL_BOOT"
+echo " - other arguments: ${OTHER_ARGUMENTS[*]}"
+echo ""
 
-
-echo "## activate minimize to dock"
-gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
-echo "## minimize to dock activated"
-
-if [ "$IS_DUAL_BOOT" = true ]; then
-  echo "## make linux listen to windows time"
-  timedatectl set-local-rtc 1 --adjust-system-clock
-  echo "## linux listens to windows time"
+if [ "$INTERNET" = true ]; then
+  echo "### UPDATE AND UPGRADE INSTALLED PACKAGES"
+  sudo apt update
+  sudo apt upgrade -y
+  echo ""
 fi
 
+echo "### COMMON SETTINGS ###"
+echo " - activate minimize to dock"
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+echo ""
 
-echo "# autoremove unnecessary packages"
-sudo apt autoremove -y
-echo "# unnecessary packages are removed"
+if [ "$IS_DUAL_BOOT" = true ]; then
+  echo "### DUAL BOOT SETTINGS ###"
+  echo " - make linux listen to windows time"
+  timedatectl set-local-rtc 1 --adjust-system-clock
+fi
+
+if [ "$INTERNET" = true ]; then
+  echo "### UPDATE AND UPGRADE INSTALLED PACKAGES"
+  sudo apt update
+  sudo apt upgrade -y
+  echo ""
+  echo "### AUTOREMOVE UNNECESSARY PACKAGES"
+  sudo apt autoremove -y
+fi
