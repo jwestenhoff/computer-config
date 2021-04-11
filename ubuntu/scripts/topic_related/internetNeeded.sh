b2a310c21112459ddb68d7eb8b5d870382ad328b
Jonas Westenhoff <jwestenhoff@web.de>
2021-04-11 14:30:18 +0200

    add packages for watching dvds and videos

--- ubuntu/scripts/topic_related/internetNeeded.sh
+++ ubuntu/scripts/topic_related/internetNeeded.sh
@@ -4,3 +4,8 @@ echo "### UPDATE AND UPGRADE INSTALLED PACKAGES"
 sudo apt update
 sudo apt upgrade -y
 echo ""
+
+# packages for watching videos and dvds
+sudo apt install ubuntu-restricted-extras -y
+sudo apt install libdvd-pkg -y
+sudo dpkg-reconfigure libdvd-pkg
