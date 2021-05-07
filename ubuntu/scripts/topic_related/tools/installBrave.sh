09be7de4fd8b1afedfe31f3534e941653f2c43e4
Jonas Westenhoff <jwestenhoff@web.de>
2021-05-07 11:17:12 +0200

    add print of purposes

--- ubuntu/scripts/topic_related/tools/installBrave.sh
+++ ubuntu/scripts/topic_related/tools/installBrave.sh
@@ -1,6 +1,8 @@
 # purpose:  installs brave
 # source:   https://brave.com/linux/
 
+echo "### INSTALL BRAVE BROWSER ###"
+
 sudo apt install apt-transport-https curl -y
 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
--- ubuntu/scripts/topic_related/tools/installSpotify.sh
+++ ubuntu/scripts/topic_related/tools/installSpotify.sh
@@ -1,6 +1,8 @@
 # purpose:  installs spotify
 # source:   https://www.spotify.com/us/download/linux/
 
+echo "### INSTALL SPOTIFY CLIENT ###"
+
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
