02d27733a15b75e2fa50a770168c904d0a577da9
Jonas Westenhoff <jwestenhoff@web.de>
2021-05-07 14:05:07 +0200

    remove unnecessary scripts

--- ubuntu/scripts/topic_related/dualBootSettings.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "### DUAL BOOT SETTINGS ###"
-echo " - make linux listen to windows time"
-timedatectl set-local-rtc 1 --adjust-system-clock
--- ubuntu/scripts/topic_related/tools/installWithSnap.sh
+++
@@ -1,5 +0,0 @@
-sudo snap install telegram-desktop
-
-sudo snap install discord
-
-sudo snap install --classic code    # VSCode
