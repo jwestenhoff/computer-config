802a777b3054224092a13b3f4fac5614d1d975a5
Jonas Westenhoff <jwestenhoff@web.de>
2021-05-07 14:07:07 +0200

    introduce setup dev

---
+++ ubuntu/scripts/setupDev.sh
@@ -0,0 +1,10 @@
+# add bin bash line
+
+echo "### INSTALL GIT ###"
+sudo apt install git -y # add hint to add global config (mail and user name)
+
+echo "### INSTALL VIM ###"
+sudo apt install vim -y
+
+echo "### INSTALL VSCode ###"
+sudo snap install --classic code    # VSCode
--- ubuntu/scripts/topic_related/tools/devInstalls.sh
+++
@@ -1,4 +0,0 @@
-# add bin bash line
-
-sudo apt install git -y # add hint to add global config (mail and user name)
-sudo apt install vim -y
