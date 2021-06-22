84490aacf8f92cc63ab1695f024273d58ffd2e28
Jonas Westenhoff <jwestenhoff@web.de>
2021-06-22 08:55:51 +0200

    add option to none private setup

--- ubuntu/scripts/installAll.sh
+++ ubuntu/scripts/installAll.sh
@@ -1,9 +1,33 @@
 pathOfThisFile="./installAll.sh"
+pathOfPrivateSetupFile="./installPrivateSetup.sh"
+nonePrivate = false
+
+OTHER_ARGUMENTS=()
+
+echo "# read and process arguments..."
+for arg in "$@"; do
+  case $arg in
+  -np | --none-private)
+    nonePrivate=true
+    shift # Remove argument from processing
+    ;;
+  *)
+    OTHER_ARGUMENTS+=("$1")
+    shift # Remove argument from processing
+    ;;
+  esac
+done
+echo ""
 
 echo "### INSTALL ALL ###"
+echo " - none-private: $nonePrivate"
+echo " - other arguments: ${OTHER_ARGUMENTS[*]}"
+echo ""
 
 for filePath in ./install*.sh; do
-    if [ $filePath != $pathOfThisFile ]
+    notPrivateSetupAndIsPrivateSetupFile=(nonePrivate && $filePath == $pathOfPrivateSetupFile)
+
+    if [ $filePath != $pathOfThisFile && !(notPrivateSetupAndIsPrivateSetupFile) ]
     then
         chmod +x "$filePath" # make file executable
 	    "$filePath" # execute file
