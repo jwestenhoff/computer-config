926f3aa75e139f5a0c07bfde9dda7a5d88e99664
Jonas Westenhoff <jwestenhoff@web.de>
2021-06-22 19:36:53 +0200

    add moves to current directory to cover relative file paths

--- ubuntu/scripts/default/installSignal.sh
+++ ubuntu/scripts/default/installSignal.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# do the following two steps to be able to execute script from everywhere and use relative paths
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
 # imports
 . "../printHeading.sh" # import print heading function
 
