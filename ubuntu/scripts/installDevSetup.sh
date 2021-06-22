4bf6eb6e89358729a1f373848c5654b31a864924
Jonas Westenhoff <jwestenhoff@web.de>
2021-06-22 09:06:54 +0200

    execute install files from dev folder in dev setup

--- ubuntu/scripts/installDevSetup.sh
+++ ubuntu/scripts/installDevSetup.sh
@@ -8,3 +8,8 @@ sudo apt install vim -y
 
 echo "### INSTALL VSCode ###"
 sudo snap install --classic code
+
+for filePath in ./dev/install*.sh; do
+    chmod +x "$filePath" # make file executable
+	"$filePath" # execute file
+done
