885e1c875d7b1f00cf4b51f9acb50327c51e60b4
Jonas Westenhoff <jwestenhoff@web.de>
2022-05-02 11:36:45 +0200

    feat: add private install of apt packages

--- linux/ubuntu-scripts/installPrivateSetup.sh
+++ linux/ubuntu-scripts/installPrivateSetup.sh
@@ -3,12 +3,15 @@
 ubuntuScriptPath=$1
 myFolder=private
 snapPackages=("discord" "telegram-desktop" "slack --classic")
+aptPackages=()
 
 source ${ubuntuScriptPath}/utilFunctions.sh
 
 echo "# INSTALLING ${myFolder^^} SETUP ..."
 
 executeSnapInstalls "${snapPackages[@]}"
+
+executeAptInstalls "${aptPackages[@]}"
 
 executeInstallScriptsInFolder ${ubuntuScriptPath}/$myFolder
 status=$?
