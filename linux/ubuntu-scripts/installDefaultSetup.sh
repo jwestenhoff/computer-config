29bcb32a5b4f6a6ee3dc7b9293894bd77731fbe9
Jonas Westenhoff <jwestenhoff@web.de>
2022-05-03 10:16:18 +0200

    fix: fit execution of minimize

--- linux/ubuntu-scripts/installDefaultSetup.sh
+++ linux/ubuntu-scripts/installDefaultSetup.sh
@@ -13,11 +13,9 @@ echo "# INSTALLING ${myFolder^^} SETUP ..."
 sudo apt update
 sudo apt upgrade -y
 
-# if [[ $status == 0 ]]; then # TODO: find solution - doesn't work for github action
-#     # activate minimize to dock
+# activate minimize to dock
+# (jenkins has no user interface -> always exit != 0 -> status not tracked)
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
-#     status=$?
-# fi
 
 if [[ $status == 0 ]]; then
     executeSnapInstalls "${snapPackages[@]}"
